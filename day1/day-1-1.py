472c6e44992fd884df0fdf963459f8709ca53822
Jim Weitzel <jtwtzl@gmail.com>
2021-12-01 11:41:10 -0500

    Solved solution for day 1-1

--- day1/day-1-1.py
+++ day1/day-1-1.py
@@ -1,2 +1,14 @@
+previous_number = 0
+counter = 0
 with open('input1.txt') as f:
-    line = f.readline()
+    for line in f:
+        number = int(line)
+        
+        if number > previous_number:
+            counter += 1
+
+        previous_number = number
+
+#subtract 1 since the first depth will always be higher than 0
+counter = counter - 1
+print('The depth increased this many times: ', counter)
