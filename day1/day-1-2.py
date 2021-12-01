9ca997a9076131c2e2521cad048ae1bd88f2f161
Jim Weitzel <jtwtzl@gmail.com>
2021-12-01 14:20:29 -0500

    Day 1 Part 2 Solution

--- day1/day-1-2.py
+++ day1/day-1-2.py
@@ -1,14 +1,21 @@
-previous_number = 0
+previous_sum = 0
 counter = 0
 with open('input1.txt') as f:
-    for line in f:
-        number = int(line)
-        
-        if number > previous_number:
+    lines = f.readlines()
+
+    #subtract 2 from length of file since we need to grab groupings of 3
+    for i in range(len(lines) - 2):
+        int1 = int(lines[i])
+        int2 = int(lines[i + 1])
+        int3 = int(lines[i + 2])
+
+        total = int1 + int2 + int3
+
+        if total > previous_sum:
             counter += 1
 
-        previous_number = number
+        previous_sum = total
 
-#subtract 1 since the first depth will always be higher than 0
+#subtract 1 since the first total will always be higher than 0
 counter = counter - 1
-print('The depth increased this many times: ', counter)
+print('The triple sum increased this many times: ', counter)
